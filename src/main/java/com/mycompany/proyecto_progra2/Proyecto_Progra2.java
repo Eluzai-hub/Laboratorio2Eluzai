8fcdcbec8e07bbae0fb078ac0150e6b01a22bd4e
Eluzai <Eluzai@192.168.136.43>
2026-04-19 00:28:06 -0600

    Paginas principales de (log,menu,mantenimiento)

---
+++ src/main/java/com/mycompany/proyecto_progra2/Proyecto_Progra2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_progra2;
+
+/**
+ *
+ * @author Eluzai
+ */
+public class Proyecto_Progra2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
